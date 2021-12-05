fcaf7742ac7508019d95f58056ee242f6c859863
liuda <liudmyla.iovenko@gmail.com>
2021-12-05 13:02:47 -0500

    optimazed logic

--- Week2_HW1_Strings/1_palindrom.js
+++ Week2_HW1_Strings/1_palindrom.js
@@ -1,10 +1,14 @@
 // 1. Write a program to find if a user entered string is a palindrome or not (for example "racecar" is a palindrome)
 
-let prompt = require('prompt-sync')();
-let userText = prompt('Enter some text: ');
-let result = "";
-for (let i = userText.length; i > 0; i--) {
-    result += userText.charAt(i - 1);
-}
-(result.toLowerCase() === userText.toLowerCase()) ? console.log(userText + ' is a palindrom string') : console.log(userText + ' is not a palindrom string');
+const prompt = require('prompt-sync')();
+const userText = prompt('Enter some text: ');
+let isPalindrome = true;
 
+// split the text in half and do a mirrored character comparison
+for (let i = 0, n = userText.length; i < Math.floor(n / 2); i++) {
+    if (userText.charAt(i).toLowerCase() !== userText.charAt(n - 1 - i).toLowerCase()) {
+        isPalindrome = false;
+        break;
+    }
+}
+(isPalindrome) ? console.log(userText + ' is a palindrome') : console.log(userText + ' is NOT a palindrome');
