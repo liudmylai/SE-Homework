3cdba3a9feec5362b13773cd9b29ba8501b0b058
liuda <liudmyla.iovenko@gmail.com>
2021-12-13 17:19:44 -0500

    optimize logic using 'reduce' function

--- Week2_HW2_Arrays/1_addUp.js
+++ Week2_HW2_Arrays/1_addUp.js
@@ -1,9 +1,12 @@
 // 1. Write a program to add up all the elements of a number array.
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-const addUp = arr => {
-    let result = 0;
-    // Call a function for each element in arr to add an element value to result
-    arr.forEach((item) => result += item);
-    return result;
-}
+// const addUp = arr => {
+//     let result = 0;
+//     // Call a function for each element in arr to add an element value to result
+//     arr.forEach((item) => result += item);
+//     return result;
+// }
+
+// Optimize logic using 'reduce' function
+const addUp = arr => arr.reduce((sum, number) => sum + number, 0);
 console.log(addUp(numbers));
