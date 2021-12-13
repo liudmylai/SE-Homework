21de2273c713fef5f490d9645b06a7e8a0989038
liuda <liudmyla.iovenko@gmail.com>
2021-12-13 17:34:28 -0500

    optimize logic using 'reduce' function

--- Week2_HW2_Arrays/3_longestEl.js
+++ Week2_HW2_Arrays/3_longestEl.js
@@ -1,15 +1,19 @@
 // 3. Write a program to find the longest element in a string array.
 const colors = ['red', 'blue', 'yellow', 'black', 'green'];
-const getLongestElement = arr => {
-    let result = '';
-    let tmp = 0;
-    // Call a function for each element in arr to find the longest element and set it to result
-    arr.forEach((value) => {
-        if (tmp < value.length) {
-            tmp = value.length;
-            result = value;
-        }
-    });
-    return result;
-}
+// const getLongestElement = arr => {
+//     let result = '';
+//     let tmp = 0;
+//     // Call a function for each element in arr to find the longest element and set it to result
+//     arr.forEach((value) => {
+//         if (tmp < value.length) {
+//             tmp = value.length;
+//             result = value;
+//         }
+//     });
+//     return result;
+// }
+
+// Optimize logic using 'reduce' function
+const getLongestElement = arr => arr.reduce((result, element) => (result.length < element.length) ? element : result, '');
 console.log(getLongestElement(colors));
+
