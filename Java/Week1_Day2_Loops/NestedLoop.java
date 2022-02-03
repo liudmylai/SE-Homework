eb8326185f458ff2e755502780b0e718d82cd490
liuda <liudmyla.iovenko@gmail.com>
2022-02-03 17:24:01 -0500

    fixed spacing between columns

--- Java/Week1_Day2_Loops/NestedLoop.java
+++ Java/Week1_Day2_Loops/NestedLoop.java
@@ -1,9 +1,9 @@
 //  Write a for loop to display the following outputs: 
-//  1                    11111
-//  22                   2222
-//  333                  333
-//  4444                 44
-//  55555                5
+//   1               11111
+//   22              2222
+//   333             333
+//   4444            44
+//   55555           5
 
 public class NestedLoop {
     public static void main (String[] args) {
@@ -11,7 +11,9 @@ public class NestedLoop {
             for(int j = 1; j <= i; j++){
                 System.out.print(i);
             }
-            System.out.print("          ");
+            for(int j = i; j <= 15; j++){
+                System.out.print(" ");
+            }
             for(int j = i; j <= 5; j++){
                 System.out.print(i);
             }
