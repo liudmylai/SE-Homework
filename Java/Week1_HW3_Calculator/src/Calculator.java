e725ae1c0bb6abaef31ab0d8919cd00400633e18
liudmylai <liudmyla.iovenko@gmail.com>
2022-02-08 18:27:39 -0500

    take input inside the methods to perform the related operation

--- Java/Week1_HW3_Calculator/src/Calculator.java
+++ Java/Week1_HW3_Calculator/src/Calculator.java
@@ -1,25 +1,41 @@
 import java.util.Scanner;
 
 public class Calculator {
-	static double Add(double num1, double num2) {
+	static Scanner scan = new Scanner(System.in);
+
+	static double Add() {
+		double num1 = getFirstNumber();
+		double num2 = getSecondNumber();
 		return num1 + num2;
 	}
-	static double Subtract(double num1, double num2) {
+	static double Subtract() {
+		double num1 = getFirstNumber();
+		double num2 = getSecondNumber();
 		return num1 - num2;
 	}
-	static double Multiply(double num1, double num2) {
+	static double Multiply() {
+		double num1 = getFirstNumber();
+		double num2 = getSecondNumber();
 		return num1 * num2;
 	}
-	static double Divide(double num1, double num2) {
+	static double Divide() {
+		double num1 = getFirstNumber();
+		double num2 = getSecondNumber();
 		return num1 / num2;
 	}
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		System.out.print("Enter the first number: ");
-		double num1 = scan.nextDouble();
+	
+	static double getFirstNumber() {
+		System.out.print("\nEnter the first number: ");
+		return scan.nextDouble();
+	}
+
+	static double getSecondNumber() {
 		System.out.print("\nEnter the second number: ");
-		double num2 = scan.nextDouble();
-		System.out.println("\nOptions:\n1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Exit");
+		return scan.nextDouble();
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("OPTIONS\n1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Exit");
 		boolean isInvalidOption;
 		double result = 0;
 		do {
@@ -28,16 +44,16 @@ public class Calculator {
 			int option = scan.nextInt();
 			switch(option) {
 			case 1:
-				result = Add(num1, num2);
+				result = Add();
 				break;
 			case 2:
-				result = Subtract(num1, num2);
+				result = Subtract();
 				break;
 			case 3:
-				result = Multiply(num1, num2);
+				result = Multiply();
 				break;
 			case 4:
-				result = Divide(num1, num2);
+				result = Divide();
 				break;
 			case 5:
 				System.exit(0);
