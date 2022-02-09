0df8650abe59b2fc079181732f9867936705b132
liudmylai <liudmyla.iovenko@gmail.com>
2022-02-09 10:26:06 -0500

    count numbers of characters without length()

--- Java/Week1_Day2_Strings/TotalCharacters.java
+++ Java/Week1_Day2_Strings/TotalCharacters.java
@@ -6,7 +6,13 @@ public class TotalCharacters {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter your text:");
 		String text = scan.nextLine();
-		System.out.println("The total numbers of characters is: " + text.length());
+		String temp = text;
+		int count = 0;
+		while (!temp.isEmpty()) {
+			temp = temp.substring(1);
+			count++;
+		}
+		System.out.println("The total numbers of characters is: " + count);
 		scan.close();
 	}
 }
